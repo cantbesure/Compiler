4cb91aa0667ad0fd15c1bd6ef5d564dd57b9ac40
cantbesure <surelee0@gmail.com>
2015-05-31 18:19:06 +0800

    read rules

--- lexer/lexer.cpp
+++ lexer/lexer.cpp
@@ -1,9 +1,55 @@
 #include <cstdio>
 #include <iostream>
+#include <map>
+#include <vector>
+#include <cstring>
+#include <cstdlib>
 using namespace std;
+map < string ,int> symbols;
+vector<string> rules;
+int sym_cnt=0;
 
+void rules_preload()
+{
+  FILE * pFile;
+  char symbol[200];//used to temporarily store 
+  char rule[500];
+  pFile =fopen("rules","r");
+  while (!feof(pFile))
+    {
+      fscanf(pFile,"%[^:]%*c",symbol);//use regex
+      char c;
+      int cnt=0;
+      while ((c=fgetc(pFile))!=EOF && c!=';')
+	{
+
+	  if (isspace(c))
+	    continue;
+	  rule[cnt++]=c;
+	  //if meet '\'
+	  if (c=='\\')
+	    {
+	      c=fgetc(pFile);
+	      rule[cnt++]=c;
+	    }
+	}
+      rule[cnt]=0;
+      //cout<<symbol<<endl;
+      symbols.insert(make_pair(string(symbol),sym_cnt++));
+      rules.push_back(string(rule));
+    }
+}
+
+void generateNFA(graph& nfa)
+{
+  
+
+
+
+}
 int main()
 {
-  cout<<"hello world!"<<endl;
+  rules_preload();
+  
   return 0;
 }
--- lexer/lexer.h
+++ lexer/lexer.h
@@ -2,6 +2,29 @@
 #define LEXER_H
 
 #include <iostream>
-#include <vector>
+#include <map>
 using namespace std;
 
+class edge{
+ public :
+  int val;
+  char got;
+  edge (int v,char g)
+    {
+      val=v;
+      got=g;
+    }
+
+}
+
+class graph{
+
+ private:
+  map <int , map<int ,edge> > data;
+ public: 
+ void addedge(int u,int v,char c);
+ map <int ,edge> begin_map(int u);
+ int new_node();
+}
+
+void pre_file_load();
